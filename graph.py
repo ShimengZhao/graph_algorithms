b90bc67e838679df1781f36917b154bcf0ce3500
shimengzhao <shimeng.zhao@thoughtworks.com>
2022-05-28 18:04:54 +0800

    add generate random graph method; import from file auto detect node number.

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 import csv
+import random
 from typing import List
 
 
@@ -28,20 +29,56 @@ class Graph:
         else:
             return False
 
+    def set_key_map(self, key_map):
+        self.key_map = key_map
+
+    def set_id_map(self, id_map):
+        self.id_map = id_map
+
+    def get_distance_by_name(self, v1, v2):
+        return self.adjacency_matrix[self.key_map[v1]][self.key_map[v2]]
+
+    def export_graph(self, destination_path):
+        with open(destination_path, "w+") as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerow(['start', 'end', 'weight'])
+            for i in range(len(self.adjacency_matrix)):
+                for j in range(len(self.adjacency_matrix)):
+                    if self.adjacency_matrix[i][j] != -1:
+                        csv_writer.writerow([i, j, self.adjacency_matrix[i][j]])
+
     @classmethod
-    def from_csv(cls, number_of_vertices, file_path, directed=True):
-        with open(file_path, 'r') as f:
-            edges = csv.reader(f)
-            next(edges)
-            graph = cls(number_of_vertices, directed)
-            for edge in edges:
-                graph.add_edge(
-                    (int(edge[0]), int(edge[1]), float(edge[2]))
-                )
+    def from_csv(cls, file_path, directed=True):
+        edge_list, v_set = cls._get_vertex_set_and_edge_list(file_path)
+        graph = cls(len(v_set), directed)
+        for edge in edge_list:
+            graph.add_edge(
+                (int(edge[0]), int(edge[1]), float(edge[2]))
+            )
         return graph
 
     @classmethod
     def from_named_nodes_csv(cls, file_path, directed=True):
+        edge_list, v_set = cls._get_vertex_set_and_edge_list(file_path)
+
+        key_map = dict()
+        id_map = ['' for _ in range(len(v_set))]
+        for i, v in enumerate(v_set):
+            key_map[v] = i
+            id_map[i] = v
+
+        graph = cls(number_of_vertices=len(v_set), directed=directed)
+        graph.set_key_map(key_map)
+        graph.set_id_map(id_map)
+        for edge in edge_list:
+            graph.add_edge(
+                (key_map[edge[0]], key_map[edge[1]], float(edge[2]))
+            )
+        return graph
+
+    @staticmethod
+    def _get_vertex_set_and_edge_list(file_path):
+        # todo: 2. auto check whether the given file is directed or not
         with open(file_path, 'r') as f:
             edges = csv.reader(f)
             next(edges)
@@ -52,30 +89,14 @@ class Graph:
                 v_set.add(edge[0])
                 v_set.add(edge[1])
                 edge_list.append(edge)
+        return edge_list, v_set
 
-            key_map = dict()
-            id_map = ['' for _ in range(len(v_set))]
-            for i, v in enumerate(v_set):
-                key_map[v] = i
-                id_map[i] = v
-
-            graph = cls(number_of_vertices=len(v_set), directed=directed)
-            graph.set_key_map(key_map)
-            graph.set_id_map(id_map)
-            for edge in edge_list:
-                graph.add_edge(
-                    (key_map[edge[0]], key_map[edge[1]], float(edge[2]))
-                )
-        return graph
-
-    def set_key_map(self, key_map):
-        self.key_map = key_map
-
-    def set_id_map(self, id_map):
-        self.id_map = id_map
-
-    def get_distance_by_name(self, v1, v2):
-        return self.adjacency_matrix[self.key_map[v1]][self.key_map[v2]]
-
-    # todo: 1. add export to file function
-    # todo: 2. add generate random graph function
+    @classmethod
+    def generate_random_graph(cls, number_of_vertices, weight_range: tuple):
+        g = cls(number_of_vertices=number_of_vertices)
+        for i in range(len(g.adjacency_matrix)):
+            for j in range(len(g.adjacency_matrix)):
+                if i != j:
+                    g.adjacency_matrix[i][j] = round(
+                        weight_range[0] + (weight_range[1] - weight_range[0]) * random.random(), 2)
+        return g
--- test/test_graph.py
+++ test/test_graph.py
@@ -1,3 +1,5 @@
+import csv
+import os
 import unittest
 from os import path
 
@@ -11,7 +13,7 @@ class TestGraph(unittest.TestCase):
         self.assertEqual(10, g.get_distance_by_name('E', 'D'))
 
     def test_should_initiate_graph_from_file_successfully(self):
-        g = Graph.from_csv(number_of_vertices=9, file_path=path.join('test_resources', 'graph.csv'), directed=False)
+        g = Graph.from_csv(file_path=path.join('test_resources', 'graph.csv'), directed=False)
         self.assertEqual(12, g.adjacency_matrix[5][8])
 
     def test_should_create_adjacency_matrix_of_the_give_size(self):
@@ -42,6 +44,24 @@ class TestGraph(unittest.TestCase):
         g = Graph(4)
         with self.assertRaises(ValueError):
             g.add_edge((1, 4, 10))
+
+    def test_should_generate_random_graph_correctly(self):
+        g = Graph.generate_random_graph(number_of_vertices=10, weight_range=(10, 1000))
+        self.assertEqual(-1, g.adjacency_matrix[0][0])
+        self.assertNotEqual(-1, g.adjacency_matrix[0][3])
+
+    def test_should_export_graph_correctly(self):
+        g_export = Graph(4)
+        edge_tuple_list = [(1, 3, 9), (2, 0, 6)]
+        g_export.add_edges(edge_tuple_list=edge_tuple_list)
+
+        dest_path = 'export_result.csv'
+        g_export.export_graph(destination_path=dest_path)
+        g_import = Graph.from_csv(file_path=dest_path, directed=True)
+        self.assertEqual(9, g_import.adjacency_matrix[1][3])
+        self.assertEqual(6, g_import.adjacency_matrix[2][0])
+
+        os.remove(dest_path)
 
 
 if __name__ == '__main__':
